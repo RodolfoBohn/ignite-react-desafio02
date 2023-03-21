4ddf07d7c75973cc7ee8223730016d17ca1f32fe
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-03-21 20:29:54 -0300

    Adicao styled components e criacao de tema default

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components'
+
+const Componente1 = styled.h1`
+  font-family: ${(props) => props.theme.fontFamily.roboto};
+  font-weight: ${(props) => props.theme.fontWeight};
+  font-size: ${(props) => props.theme.fontSize[100]};
+`
+
 function App() {
-  return <h1> Opa</h1>
+  return (
+    <>
+      <Componente1>Fala galera</Componente1>
+    </>
+  )
 }
 
 export default App
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,47 @@
+export const defaultTheme = {
+  colors: {
+    yellow: {
+      light: '#f1e9c9',
+      medium: '#dbac2c',
+      dark: '#c47f17',
+    },
+    purple: {
+      light: '#ebe5f9',
+      medium: '#8047f8',
+      dark: '#4b2995',
+    },
+    base: {
+      title: '#272221',
+      subtitle: '#403937',
+      text: '#574f4d',
+      label: '#8d8686',
+      hover: '#d7d5d5',
+      button: '#e6e5e5',
+      input: '#ededed',
+      card: '#f3f2f2',
+      background: '#fafafa',
+      white: '#ffffff',
+    },
+  },
+  fontSize: {
+    100: '0.625rem',
+    200: '0.75rem',
+    300: '0.875rem',
+    400: '1rem',
+    500: '1.125rem',
+    600: '1.25rem',
+    750: '1.5rem',
+    800: '2rem',
+    1200: '3rem',
+  },
+  fontWeight: {
+    regular: 400,
+    bold: 700,
+    extraBold: 800,
+  },
+  fontFamily: {
+    baloo_2: "'Baloo 2', cursive",
+    roboto: "'Roboto', sans-serif",
+  },
+  lineHeight: { 200: '1.3', 400: '1.6' },
+}
