53e6f5b5906d4341d5b5fedd1ddb4ec746676a09
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-03-25 10:49:02 -0300

    Finalizacao pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,159 @@
 import { ShoppingCart, Timer, Package, Coffee } from 'phosphor-react'
-import { BannerWrapper, TitleWrapper, ItemsWrapper, Item } from './styles'
+import {
+  BannerWrapper,
+  TitleWrapper,
+  ItemsWrapper,
+  Item,
+  CoffeesListWrapper,
+  CoffeeCard,
+  Tag,
+} from './styles'
 import coffeeBanner from '../../assets/coffee-banner.png'
+import arabe from '../../assets/coffees-images/arabe.png'
+import cafeComLeite from '../../assets/coffees-images/cafe-com-leite.png'
+import capuccino from '../../assets/coffees-images/capuccino.png'
+import chocolateQuente from '../../assets/coffees-images/chocolate-quente.png'
+import cubano from '../../assets/coffees-images/cubano.png'
+import expressoAmericano from '../../assets/coffees-images/expresso-americano.png'
+import expressoCremoso from '../../assets/coffees-images/expresso-cremoso.png'
+import expressoGelado from '../../assets/coffees-images/expresso-gelado.png'
+import expressoTradicional from '../../assets/coffees-images/expresso-tradicional.png'
+import havaiano from '../../assets/coffees-images/havaiano.png'
+import irlandes from '../../assets/coffees-images/irlandes.png'
+import latte from '../../assets/coffees-images/latte.png'
+import mocaccino from '../../assets/coffees-images/mocaccino.png'
+import macchiato from '../../assets/coffees-images/macchiato.png'
+
+interface CoffeeProps {
+  name: string
+  description: string
+  tags: string[]
+  value: number
+  image: string
+}
+
+const EXPRESSO_TRADICIONAL: CoffeeProps = {
+  name: 'Expresso Tradicional',
+  description: 'O tradicional café feito com água quente e grãos moídos',
+  tags: ['tradicional'],
+  value: 9.9,
+  image: expressoTradicional,
+}
+
+const EXPRESSO_AMERCANO: CoffeeProps = {
+  name: 'Expresso Americano',
+  description: 'Expresso diluído, menos intenso que o tradicional',
+  tags: ['tradicional'],
+  value: 9.9,
+  image: expressoAmericano,
+}
+const EXPRESSO_CREMOSO: CoffeeProps = {
+  name: 'Expresso Cremoso',
+  description: 'Café expresso tradicional com espuma cremosa',
+  tags: ['tradicional'],
+  value: 9.9,
+  image: expressoCremoso,
+}
+const EXPRESSO_GELADO: CoffeeProps = {
+  name: 'Expresso Gelado',
+  description: 'Bebida preparada com café expresso e cubos de gelo',
+  tags: ['tradicional', 'gelado'],
+  value: 9.9,
+  image: expressoGelado,
+}
+const CAFE_COM_LEITE: CoffeeProps = {
+  name: 'Café com Leite',
+  description: 'Meio a meio de expresso tradicional com leite vaporizado',
+  tags: ['tradicional', 'com leite'],
+  value: 9.9,
+  image: cafeComLeite,
+}
+
+const LATTE: CoffeeProps = {
+  name: 'Latte',
+  description:
+    'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+  tags: ['tradicional', 'com leite'],
+  value: 9.9,
+  image: latte,
+}
+
+const CAPUCCINO: CoffeeProps = {
+  name: 'Capuccino',
+  description:
+    'Bebida com canela feita de doses iguais de café, leite e espuma',
+  tags: ['tradicional', 'com leite'],
+  value: 9.9,
+  image: capuccino,
+}
+const MACCHIATO: CoffeeProps = {
+  name: 'Macchiato',
+  description: 'Café expresso misturado com um pouco de leite quente e espuma',
+  tags: ['tradicional', 'com leite'],
+  value: 9.9,
+  image: macchiato,
+}
+const MOCACCINO: CoffeeProps = {
+  name: 'Mocaccino',
+  description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+  tags: ['tradicional', 'com leite'],
+  value: 9.9,
+  image: mocaccino,
+}
+const CHOCOLATE_QUENTE: CoffeeProps = {
+  name: 'Chocolate Quente',
+  description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+  tags: ['tradicional', 'com leite'],
+  value: 9.9,
+  image: chocolateQuente,
+}
+const CUBANO: CoffeeProps = {
+  name: 'cubano',
+  description:
+    'Drink gelado de café expresso com rum, creme de leite e hortelã',
+  tags: ['especial', 'alcoólico', 'gelado'],
+  value: 9.9,
+  image: cubano,
+}
+const HAVAIANO: CoffeeProps = {
+  name: 'Havaiano',
+  description: 'Bebida adocicada preparada com café e leite de coco',
+  tags: ['especial'],
+  value: 9.9,
+  image: havaiano,
+}
+const ARABE: CoffeeProps = {
+  name: 'Árabe',
+  description: 'Bebida preparada com grãos de café árabe e especiarias',
+  tags: ['especial'],
+  value: 9.9,
+  image: arabe,
+}
+
+const IRLANDES: CoffeeProps = {
+  name: 'Irlandes',
+  description: 'Bebida preparada com grãos de café árabe e especiarias',
+  tags: ['especial', 'alcoólico'],
+  value: 9.9,
+  image: irlandes,
+}
+
+export const coffeesList: CoffeeProps[] = [
+  EXPRESSO_TRADICIONAL,
+  EXPRESSO_AMERCANO,
+  EXPRESSO_CREMOSO,
+  EXPRESSO_GELADO,
+  CAFE_COM_LEITE,
+  LATTE,
+  CAPUCCINO,
+  MACCHIATO,
+  MOCACCINO,
+  CHOCOLATE_QUENTE,
+  CUBANO,
+  HAVAIANO,
+  ARABE,
+  IRLANDES,
+]
 
 export const Home = () => {
   return (
@@ -43,6 +196,36 @@ export const Home = () => {
         </div>
         <img src={coffeeBanner} alt="Banner image" />
       </BannerWrapper>
+      <CoffeesListWrapper>
+        <h2>Nossos cafés</h2>
+        <div>
+          {coffeesList.map((coffee) => (
+            <CoffeeCard key={coffee.name}>
+              <img src={coffee.image} alt={coffee.name} />
+              <div>
+                {coffee.tags.map((tag) => (
+                  <Tag key={tag}>{tag.toUpperCase()}</Tag>
+                ))}
+              </div>
+              <h3>{coffee.name}</h3>
+              <p>{coffee.description}</p>
+              <form>
+                <label htmlFor="quantity">
+                  <span>R$ </span>
+                  {coffee.value.toLocaleString('pt-br', {
+                    minimumFractionDigits: 2,
+                  })}
+                </label>
+
+                <input id="quantity" type={'number'} min={0} />
+                <button type="submit">
+                  <ShoppingCart weight="fill" size={22} />
+                </button>
+              </form>
+            </CoffeeCard>
+          ))}
+        </div>
+      </CoffeesListWrapper>
     </>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -73,3 +73,100 @@ export const Item = styled.div<ItemsWrapperProps>`
     color: ${(props) => props.theme.colors.base.text};
   }
 `
+export const CoffeesListWrapper = styled.main`
+  padding: ${(props) => props.theme.space[800]} 0;
+  & > h2 {
+    font-family: ${(props) => props.theme.fontFamily.baloo_2};
+    font-size: ${(props) => props.theme.fontSize[800]};
+    color: ${(props) => props.theme.colors.base.subtitle};
+    font-weight: ${(props) => props.theme.fontWeight.extraBold};
+    margin-bottom: 3.375rem;
+  }
+
+  & > div {
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: ${(props) => props.theme.space[800]};
+    row-gap: 2.5rem;
+  }
+`
+
+export const CoffeeCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 16rem;
+  height: 19.375rem;
+  padding: ${(props) => props.theme.space[600]}
+    ${(props) => props.theme.space[750]};
+  background: ${(props) => props.theme.colors.base.card};
+  border-radius: 6px 36px 6px 36px;
+
+  & > img {
+    width: 7.5rem;
+    height: 7.5rem;
+    margin-top: -40px;
+  }
+
+  & > div {
+    margin-top: ${(props) => props.theme.space[200]};
+    margin-bottom: ${(props) => props.theme.space[400]};
+    display: flex;
+    gap: ${(props) => props.theme.space[50]};
+  }
+
+  & > h3 {
+    font-family: ${(props) => props.theme.fontFamily.baloo_2};
+    font-size: ${(props) => props.theme.fontSize[600]};
+    color: ${(props) => props.theme.colors.base.subtitle};
+  }
+
+  & > p {
+    font-size: ${(props) => props.theme.fontSize[300]};
+    color: ${(props) => props.theme.colors.base.label};
+    text-align: center;
+    margin-top: ${(props) => props.theme.space[75]};
+    margin-bottom: ${(props) => props.theme.space[800]};
+  }
+
+  & > form {
+    display: flex;
+    width: 100%;
+    & > label {
+      font-family: ${(props) => props.theme.fontFamily.baloo_2};
+      font-size: ${(props) => props.theme.fontSize[750]};
+      color: ${(props) => props.theme.colors.base.text};
+      font-weight: ${(props) => props.theme.fontWeight.extraBold};
+      margin-right: ${(props) => props.theme.space[600]};
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      & > span {
+        font-family: ${(props) => props.theme.fontFamily.roboto};
+        font-size: ${(props) => props.theme.fontSize[300]};
+      }
+    }
+    & > input {
+      width: 4.5rem;
+    }
+
+    & > button {
+      padding: ${(props) => props.theme.space[75]};
+      border-radius: 6px;
+      border: 0;
+      background: ${(props) => props.theme.colors.purple.dark};
+      color: ${(props) => props.theme.colors.base.card};
+      margin-left: ${(props) => props.theme.space[400]};
+    }
+  }
+`
+
+export const Tag = styled.span`
+  padding: ${(props) => props.theme.space[50]}
+    ${(props) => props.theme.space[75]};
+  background: ${(props) => props.theme.colors.yellow.light};
+  color: ${(props) => props.theme.colors.yellow.dark};
+  border-radius: 99999px;
+  font-size: ${(props) => props.theme.fontSize[100]};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+`
